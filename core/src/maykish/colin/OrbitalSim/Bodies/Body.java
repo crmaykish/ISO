c339d11fdbd77e625d31699599f56da7c1a00b41
Colin Maykish <crmaykish@gmail.com>
2014-08-12 21:48:57 -0400

    Simple testing and updated goals

--- core/src/maykish/colin/OrbitalSim/Bodies/Body.java
+++ core/src/maykish/colin/OrbitalSim/Bodies/Body.java
@@ -9,7 +9,7 @@ public class Body{
 	public Vector2 velocity;
 	public boolean fixed;
 	
-	protected float elasticity = 0.6f; // elasticity of collisions - 1.0 is completely elastic, 0.0 is completely inelastic, theoretically anyway
+	protected float elasticity = 0.8f; // elasticity of collisions - 1.0 is completely elastic, 0.0 is completely inelastic, theoretically anyway
 
 	public Body(float mass, int radius, Vector2 initPosition, Vector2 initVelocity) {
 		this.mass = mass;
@@ -20,46 +20,47 @@ public class Body{
 	}
 	
 	public boolean checkCollision(Body otherBody){
+		boolean result = false;
 		float distance = otherBody.position.dst(position);
 		
-		if (distance < radius + otherBody.radius){
-			return true;
+		if (distance < (radius + otherBody.radius)){
+			result = true;
 		}
-		return false;
+		return result;
 	}
 	
-	public void reactToCollision(Body otherBody) {
-		Vector2 difference = position.cpy().sub(otherBody.position);
-		float d = difference.cpy().len();
-
-		Vector2 mtd = difference.cpy().scl(
-				(radius + otherBody.radius - d) / d);
-
-		float im1 = 1 / mass;
-		float im2 = 1 / otherBody.mass;
-
-		if (!fixed)
-			position = position.cpy().add(mtd.cpy().scl(im1 / (im1 + im2)));
-		if (!otherBody.fixed)
-			otherBody.position = otherBody.position.cpy().sub(mtd.cpy().scl(im2 / (im1 + im2)));
-
-		Vector2 v = velocity.cpy().sub(otherBody.velocity);
-		Vector2 mtdN = mtd.cpy();
-		mtdN = mtdN.nor();
-		float vn = v.cpy().dot(mtdN);
-
-		if (vn > 0.0f) {
-			return;
-		}
-
-		float i = (-(1.0f + elasticity) * vn) / (im1 + im2);
-		Vector2 impulse = mtdN.cpy().scl(i);
-
-		if (!fixed)
-			velocity = velocity.cpy().add(impulse.cpy().scl(im1));
-		if (!otherBody.fixed)
-			otherBody.velocity = otherBody.velocity.cpy().sub(impulse.cpy().scl(im2));
-	}
+//	public void reactToCollision(Body otherBody) {
+//		Vector2 difference = position.cpy().sub(otherBody.position);
+//		float d = difference.cpy().len();
+//
+//		Vector2 mtd = difference.cpy().scl(
+//				(radius + otherBody.radius - d) / d);
+//
+//		float im1 = 1 / mass;
+//		float im2 = 1 / otherBody.mass;
+//
+//		if (!fixed)
+//			position = position.cpy().add(mtd.cpy().scl(im1 / (im1 + im2)));
+//		if (!otherBody.fixed)
+//			otherBody.position = otherBody.position.cpy().sub(mtd.cpy().scl(im2 / (im1 + im2)));
+//
+//		Vector2 v = velocity.cpy().sub(otherBody.velocity);
+//		Vector2 mtdN = mtd.cpy();
+//		mtdN = mtdN.nor();
+//		float vn = v.cpy().dot(mtdN);
+//
+//		if (vn > 0.0f) {
+//			return;
+//		}
+//
+//		float i = (-(1.0f + elasticity) * vn) / (im1 + im2);
+//		Vector2 impulse = mtdN.cpy().scl(i);
+//
+//		if (!fixed)
+//			velocity = velocity.cpy().add(impulse.cpy().scl(im1));
+//		if (!otherBody.fixed)
+//			otherBody.velocity = otherBody.velocity.cpy().sub(impulse.cpy().scl(im2));
+//	}
 	
 	@Override
 	public boolean equals(Object obj) {
